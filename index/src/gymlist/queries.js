e7854b15841f38cd63a30851b01f2a1d2c2e3c71
VanshChandwani01 <156844488+VanshChandwani01@users.noreply.github.com>
2024-02-01 01:08:16 +0530

    queries.js

--- index/src/gymlist/queries.js
+++ index/src/gymlist/queries.js
@@ -3,7 +3,7 @@ const getGymlistinfobyid = "SELECT * FROM equipment_list WHERE item_id=$1";
 const checkItemExists = "SELECT i FROM equipment_list i WHERE i.item_name = $1";
 const addItem = "INSERT INTO equipment_list (item_id, item_name) VALUES ($1, $2)";
 const removeItem = "DELETE FROM equipment_list WHERE item_id = $1";
-const UpdateItem = "UPDATE  equipment_list SET item_name = $1 where item_id = $2";
+const UpdateItem = "UPDATE  equipment_list SET item_name = $1 WHERE item_id = $2" ;
 
 module.exports={
     getGymlist,
@@ -12,4 +12,5 @@ module.exports={
     addItem,
     removeItem,
     UpdateItem,
+ 
 };
